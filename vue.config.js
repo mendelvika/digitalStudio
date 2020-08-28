1454980e92497b14177a128d38ebc38aa371f387
Viktoriya Mendel <v.mendel@tranzzo.com>
2020-08-28 15:01:38 +0300

    fix deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  lintOnSave: false,
-  devServer: {
-    proxy: 'http://test.sharksteam.com.ua/test.php',
-}
+    lintOnSave: false,
+    devServer: {
+        proxy: 'http://test.sharksteam.com.ua/test.php',
+        publicPath: process.env.NODE_ENV === 'production' ?
+            '/digitalStudio.github.io/' : '/'
+    }
 }
