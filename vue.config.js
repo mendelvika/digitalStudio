75097093f1554db6b562a566335a2acd4e47dc14
Viktoriya Mendel <v.mendel@tranzzo.com>
2020-09-28 10:57:41 +0300

    fix deploy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
     lintOnSave: false,
     devServer: {
         proxy: 'http://test.sharksteam.com.ua/test.php',
-        publicPath: process.env.NODE_ENV === 'production' ?
-            '/digitalStudio/' : '/'
+        publicPath: '/digitalStudio/'
     }
 }
